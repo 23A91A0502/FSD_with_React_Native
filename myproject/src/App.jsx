54a341e7bdd9279db9bee27a121e88cc85f68978
23A91A0502 <23a91a0502@aec.edu.in>
2025-09-11 15:08:27 +0530

    Initial commit

---
+++ myproject/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Name from './Name'
+import Cards from './Cards'
+function App() {
+  
+
+  return (
+    <>
+      <Cards/>
+    </>
+  )
+}
+
+export default App
---
+++ myproject/src/Name.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Name = () => {
+    return(
+        <input type="text"/>
+    )
+}
+export default Name;
