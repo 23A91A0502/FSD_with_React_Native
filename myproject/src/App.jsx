0be40a4d0ac44e2ea44103739212a48964e2cfce
23A91A0502 <23a91a0502@aec.edu.in>
2025-09-21 09:25:00 +0530

    hooks task on generate cards

--- myproject/src/App.jsx
+++ myproject/src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -5,20 +6,55 @@ import './App.css'
 import Name from './Name'
 import Cards from './Cards'
 function App() {
-  const [x , setx] = useState(1);
-  const Change = () => {
-      if(x==true){
-        setx(false);
-      }
-      else{
-        setx(true);
-      }
-  }
-
+  const fun = () => {
+    setData([...Data,userData]);
+    userData={
+       "firstname":"",
+       "lastname":"",
+       "email":""
+    };
+  };
+  var userData = {
+    "firstname":"",
+    "lastname":"",
+    "email":""
+  };
+  const [Data,setData] = useState([]);
+  const getData = (key,event) => {
+      userData[key]=event.target.value;
+  };
   return (
     <>
-      {x==true ? <h1>Hello</h1> : <h1>Bye</h1>}
-      <button onClick={Change}>Change State</button>
+    <div className="form-body">
+      <form>
+        <div>
+          <label>Firstname: </label>
+          <input type='text' onChange={(event)=>getData("firstname",event)}/>
+        </div>
+        <div>
+          <label>Lastname: </label>
+          <input type='text' onChange={(event)=>getData("lastname",event)}/>
+        </div>
+        <div>
+          <label>Email: </label>
+          <input type='email' onChange={(event)=>getData("email",event)}/>
+        </div>
+      </form>
+      <button onClick={fun}>Add Data</button>
+      </div>
+      <div className="parent">
+        {
+        Data.map((ele,index)=>{
+          return (
+           <div className="child">
+             <div>Firstname:{ele.firstname}</div>
+             <div>Lastname:{ele.lastname}</div>
+             <div>Email:{ele.email}</div>
+           </div>
+          );
+        })
+        }
+      </div>
     </>
   )
 }
